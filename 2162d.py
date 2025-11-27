e7c30bcb34b481246df3ebce38ea7f96584aa326
Kevin Zhang <kevzhang2022@gmail.com>
2025-11-27 10:33:21 -0600

    Working on 2162d

---
+++ 2162d.py
@@ -0,0 +1,57 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+II = lambda: int(input())
+LII = lambda: list(map(int, input().split()))
+pf = lambda x: print(x, flush=True)
+
+def query(l: int, r: int) -> tuple[int, int]:
+    pf(f'1 {l} {r}')
+    p = II()
+    pf(f'2 {l} {r}')
+    a = II()
+    return p, a
+
+def solve():
+    n = II()
+
+    # Starting point for the search for the right boundary
+    # Update this whenever we find a new rightmost mismatch interval
+    next_l = 2
+
+    l, r = 1, n
+    while l < r:
+        m = (l + r) // 2
+        p, a = query(l, m)
+        if p != a:
+            # left boundary is in [l, m]
+            r = m
+        else:
+            # left boundary is in (m, r]
+            l = m + 1
+            next_l = max(next_l, l + 1)
+    left_bound = l
+
+    # Edge case: the right boundary extends to the last element
+    p, a = query(n, n)
+    if p != a:
+        return left_bound, n
+
+    # Now I'm searching for the first non-different value
+    l, r = next_l, n
+    while l < r:
+        m = (l + r) // 2
+        p, a = query(l, m)
+        if p != a:
+            l = m + 1
+        else:
+            r = m
+
+    return left_bound, l - 1
+
+def main():
+    T = II()
+    for _ in range(T):
+        l, r = solve()
+        pf(f'! {l} {r}')
+main()
